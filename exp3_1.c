0973a91ae9447b4821030ccd67cd3ce780df1e95
WhiteWLf-dev <82873634+WhiteWLf-dev@users.noreply.github.com>
2025-12-26 01:11:15 +0300

    Create exp3_1.c

---
+++ exp3_1.c
@@ -0,0 +1,26 @@
+//./exp3_1 /mnt/ramdisk/exp3_1/inputs/ # exp3_1 dir on RAM
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <haproxy/cfgparse.h>
+#include "./src/cfgparse-tcp.c"
+
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+      char filename[256];
+      sprintf(filename, "/mnt/ramdisk/exp3_1/exp3cfg.%d", getpid());
+
+      FILE *fp = fopen(filename, "wb");
+      if (!fp) {
+        exit(1);
+      }
+
+      fwrite(Data, Size, 1, fp);
+      fclose(fp);
+
+      readcfgfile(filename); //target function
+
+      unlink(filename);
+}
